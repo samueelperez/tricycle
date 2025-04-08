9c4c31f6732165c00aa0de536774c8fc11fcb017
SamuelPerezTorres <spertor@epsa.upv.es>
2025-04-08 22:06:38 +0200

    pruebaaaa

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const slideImages = [
 // Textos predefinidos para cada idioma
 const heroTexts = {
   es: {
-    title: "Transformamo desechos en recursos sostenibles",
+    title: "Transformamos desechos en recursos sostenibles",
     subtitle: "Soluciones innovadoras de reciclaje para plásticos, metales y papeles que contribuyen a un futuro más verde",
     primaryBtn: "Ver Productos",
     secondaryBtn: "Más Sobre Nosotros"
